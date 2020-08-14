c42cab5dafa44ad7744c038b25d948eeb3a08dbe
trs1323 <45774674+trs1323@users.noreply.github.com>
2020-08-14 13:22:53 -0700

    Updates

---
+++ precache-manifest.01baaceaf554d436e0acff34093fe9a4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fece3d52a4fef52323293b043e68aef",
+    "url": "/TrsPortfolio/index.html"
+  },
+  {
+    "revision": "ab1f23da3bac3ccf6e39",
+    "url": "/TrsPortfolio/static/css/main.0ac92dd9.chunk.css"
+  },
+  {
+    "revision": "2068858bcdb17b538a61",
+    "url": "/TrsPortfolio/static/js/2.4385df09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TrsPortfolio/static/js/2.4385df09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab1f23da3bac3ccf6e39",
+    "url": "/TrsPortfolio/static/js/main.ede07a2e.chunk.js"
+  },
+  {
+    "revision": "df559b31e41e1cda2d91",
+    "url": "/TrsPortfolio/static/js/runtime-main.38cd9d47.js"
+  },
+  {
+    "revision": "5b6b8a5811dee79c916e4b7aab48394e",
+    "url": "/TrsPortfolio/static/media/ava.5b6b8a58.png"
+  },
+  {
+    "revision": "82d4d98919ce359881c907ad6a3be4b1",
+    "url": "/TrsPortfolio/static/media/project3.82d4d989.png"
+  },
+  {
+    "revision": "cbdfc1e98f90db73fb53b0c058ba3aba",
+    "url": "/TrsPortfolio/static/media/project4.cbdfc1e9.png"
+  },
+  {
+    "revision": "d2a9e0d205e3830229ce380ab6b7e813",
+    "url": "/TrsPortfolio/static/media/project5.d2a9e0d2.png"
+  },
+  {
+    "revision": "7216606728baa75165b11df31674f3b4",
+    "url": "/TrsPortfolio/static/media/project8.72166067.png"
+  }
+]);
